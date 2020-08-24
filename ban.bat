290534ef69ca9bd39ceb37ab5233c95245812394
Walczy <60795188+Black-DeathZ@users.noreply.github.com>
2020-08-24 17:28:08 +0100

    Update ban.bat

--- ban.bat
+++ ban.bat
@@ -1,12 +1,3 @@
-ipconfig /flushdns
-ipconfig /registerdns
-ipconfig /release
-ipconfig /renew
-netsh winsock reset
-Get-AppXPackage | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
-
-
-
 @echo off
 color 1f
 : Menu
